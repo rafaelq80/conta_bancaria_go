70555df514bbf079b8bc3674134926be8da269b8
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-09-23 21:55:51 -0300

    Refatoração das Classes Main e Controller

--- controller/contacontroller.go
+++ controller/contacontroller.go
@@ -2,171 +2,134 @@ package controller
 
 import (
 	"errors"
+	"fmt"
 
-	"github.com/rafaelq80/conta_bancaria_go/model"
 	"github.com/rafaelq80/conta_bancaria_go/repository"
 )
 
+// ContaController implementa a Interface IContaRepository
 type ContaController struct {
-	contas      []interface{}
+	contas      []Conta
 	numeroAtual int
 }
 
+// Conta interface - Simula Polimorfismo
+type Conta interface {
+	GetNumero() int
+	SetNumero(int)
+	Sacar(float64) bool
+	Depositar(float64)
+}
+
+// Método Cosbtrutor ContaController
 func NewContaController() repository.IContaRepository {
 	return &ContaController{
-		contas:      make([]interface{}, 0),
+		contas:      make([]Conta, 0),
 		numeroAtual: 0,
 	}
-
 }
 
 func (cc *ContaController) Criar(conta interface{}) error {
-	if !validarConta(conta) {
+	c, ok := conta.(Conta)
+	if !ok {
 		return errors.New("tipo de conta inválido")
 	}
 
 	cc.numeroAtual++
-
-	if setter, ok := conta.(interface{ SetNumero(int) }); ok {
-		setter.SetNumero(cc.numeroAtual)
-	} else {
-		return errors.New("a conta não possui um método SetNumero")
-	}
-
-	cc.contas = append(cc.contas, conta)
+	c.SetNumero(cc.numeroAtual)
+	cc.contas = append(cc.contas, c)
 	return nil
 }
 
 func (cc *ContaController) ListarTodas() ([]interface{}, error) {
-	return cc.contas, nil
+	listaContas := make([]interface{}, len(cc.contas))
+	for i, c := range cc.contas {
+		listaContas[i] = c
+	}
+	return listaContas, nil
 }
 
 func (cc *ContaController) BuscarPorNumero(numero int) (interface{}, error) {
 	for _, conta := range cc.contas {
-		if getNumero(conta) == numero {
+		if conta.GetNumero() == numero {
 			return conta, nil
 		}
 	}
-	return nil, errors.New("conta não encontrada")
+	return nil, fmt.Errorf("conta não encontrada: %d", numero)
 }
 
 func (cc *ContaController) Atualizar(conta interface{}) error {
-	if !validarConta(conta) {
+	c, ok := conta.(Conta)
+	if !ok {
 		return errors.New("tipo de conta inválido")
 	}
-	numero := getNumero(conta)
-	for i, c := range cc.contas {
-		if getNumero(c) == numero {
-			cc.contas[i] = conta
+
+	for i, existingConta := range cc.contas {
+		if existingConta.GetNumero() == c.GetNumero() {
+			cc.contas[i] = c
 			return nil
 		}
 	}
-	return errors.New("conta não encontrada")
+	return fmt.Errorf("conta não encontrada: %d", c.GetNumero())
 }
 
 func (cc *ContaController) Deletar(numero int) error {
-
-	for indice, conta := range cc.contas {
-		if getNumero(conta) == numero {
-			cc.contas = append(cc.contas[:indice], cc.contas[indice+1:]...)
+	for i, conta := range cc.contas {
+		if conta.GetNumero() == numero {
+			cc.contas = append(cc.contas[:i], cc.contas[i+1:]...)
 			return nil
 		}
 	}
-
-	return errors.New("conta não encontrada")
+	return fmt.Errorf("conta não encontrada: %d", numero)
 }
 
 func (cc *ContaController) Sacar(numero int, valor float64) error {
-	for _, conta := range cc.contas {
-		if getNumero(conta) == numero {
-
-			switch c := conta.(type) {
-			case *model.ContaCorrente:
-				c.Sacar(valor)
-				return nil
-			case *model.ContaPoupanca:
-				c.Sacar(valor)
-				return nil
-			}
-
-		}
+	conta, err := cc.buscarConta(numero)
+	if err != nil {
+		return err
 	}
-	return errors.New("conta não encontrada")
+
+	if !conta.Sacar(valor) {
+		return errors.New("saldo insuficiente")
+	}
+	return nil
 }
 
 func (cc *ContaController) Depositar(numero int, valor float64) error {
-	for _, conta := range cc.contas {
-		if getNumero(conta) == numero {
-
-			switch c := conta.(type) {
-			case *model.ContaCorrente:
-				c.Depositar(valor)
-				return nil
-			case *model.ContaPoupanca:
-				c.Depositar(valor)
-				return nil
-			}
-
-		}
+	conta, err := cc.buscarConta(numero)
+	if err != nil {
+		return err
 	}
-	return errors.New("conta não encontrada")
+
+	conta.Depositar(valor)
+	return nil
 }
 
 func (cc *ContaController) Transferir(numeroOrigem, numeroDestino int, valor float64) error {
-
-	contaOrigem, err := cc.BuscarPorNumero(numeroOrigem)
+	contaOrigem, err := cc.buscarConta(numeroOrigem)
 	if err != nil {
-		return errors.New("conta de origem não encontrada")
+		return fmt.Errorf("conta de origem não encontrada: %d", numeroOrigem)
 	}
 
-	contaDestino, err := cc.BuscarPorNumero(numeroDestino)
+	contaDestino, err := cc.buscarConta(numeroDestino)
 	if err != nil {
-		return errors.New("conta de destino não encontrada")
+		return fmt.Errorf("conta de destino não encontrada: %d", numeroDestino)
 	}
 
-	var resposta bool
-
-	switch co := contaOrigem.(type) {
-	case *model.ContaCorrente:
-		resposta = co.Sacar(valor)
-	case *model.ContaPoupanca:
-		resposta = co.Sacar(valor)
+	if !contaOrigem.Sacar(valor) {
+		return errors.New("saldo insuficiente na conta de origem")
 	}
 
-	if resposta == true {
-		switch cd := contaDestino.(type) {
-		case *model.ContaCorrente:
-			cd.Depositar(valor)
-		case *model.ContaPoupanca:
-			cd.Depositar(valor)
-		}
-	}else{
-		return errors.New("erro ao efetuar a transferência")
-	}
-
+	contaDestino.Depositar(valor)
 	return nil
-
 }
 
-// Funções auxiliares
-
-func getNumero(conta interface{}) int {
-
-	switch c := conta.(type) {
-	case *model.ContaCorrente:
-		return c.GetNumero()
-	case *model.ContaPoupanca:
-		return c.GetNumero()
-	default:
-		panic("tipo de conta inválido")
+// Método Auxiliar - Buscar Conta
+func (cc *ContaController) buscarConta(numero int) (Conta, error) {
+	for _, conta := range cc.contas {
+		if conta.GetNumero() == numero {
+			return conta, nil
+		}
 	}
-}
-
-func validarConta(conta interface{}) bool {
-	switch conta.(type) {
-	case model.ContaCorrente, *model.ContaCorrente, model.ContaPoupanca, *model.ContaPoupanca:
-		return true
-	default:
-		return false
-	}
+	return nil, fmt.Errorf("conta não encontrada: %d", numero)
 }
--- main.go
+++ main.go
@@ -11,380 +11,270 @@ import (
 
 	"github.com/rafaelq80/conta_bancaria_go/controller"
 	"github.com/rafaelq80/conta_bancaria_go/model"
+	"github.com/rafaelq80/conta_bancaria_go/repository"
 )
 
+type Banco interface {
+    Visualizar()
+}
+
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
-
-	var opcao = 0
-	var numero, agencia, tipo, aniversario, numeroDestino int
-	var saldo, limite, valor float64
-	var titular string
-
 	contaController := controller.NewContaController()
+	
+	inicializaContas(contaController)
 
-	contaController.Criar(model.NewContaCorrente(0, 123, 1, "João da Silva", 1000.00, 100.00))
-	contaController.Criar(model.NewContaCorrente(0, 456, 1, "Maria dos Santos", 2900.00, 300.00))
+	for {
+		
+		exibirMenu()
 
-	contaController.Criar(model.NewContaPoupanca(0, 789, 2, "Giovanna Benvenutti", 3200.00, 10))
-	contaController.Criar(model.NewContaPoupanca(0, 123, 2, "Karina Girardini", 2000.00, 15))
-
-	for true {
-
-		color.Set(color.BgBlack, color.FgYellow)
-
-		fmt.Println("*****************************************************")
-		fmt.Println("                                                     ")
-		fmt.Println("                BANCO DO BRAZIL COM Z                ")
-		fmt.Println("                                                     ")
-		fmt.Println("*****************************************************")
-		fmt.Println("                                                     ")
-		fmt.Println("            1 - Criar Conta                          ")
-		fmt.Println("            2 - Listar todas as Contas               ")
-		fmt.Println("            3 - Buscar Conta por Numero              ")
-		fmt.Println("            4 - Atualizar Dados da Conta             ")
-		fmt.Println("            5 - Apagar Conta                         ")
-		fmt.Println("            6 - Sacar                                ")
-		fmt.Println("            7 - Depositar                            ")
-		fmt.Println("            8 - Transferir valores entre Contas      ")
-		fmt.Println("            9 - Sair                                 ")
-		fmt.Println("                                                     ")
-		fmt.Println("*****************************************************")
-		fmt.Println("                                                     ")
-
-		fmt.Println("Digite a opção desejada: ")
-
-		color.Unset()
-
-		fmt.Scanln(&opcao)
-
-		if opcao == 9 {
-			color.Set(color.FgCyan)
-			fmt.Println("\nBanco do Brazil com Z - O seu Futuro começa aqui!")
-			sobre()
-			os.Exit(0)
-			color.Unset()
-		}
+		opcao := readOption()
 
 		switch opcao {
 		case 1:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nCriar Conta\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO CRIAR CONTA */
-
-			fmt.Println("\nDigite o número da agência: ")
-			fmt.Scanln(&agencia)
-
-			fmt.Print("\n")
-
-			selecionada := ""
-			prompt := &survey.Select{
-				Message: "Selecione o tipo da conta:",
-				Options: []string{"Conta Corrente", "Conta Poupança"},
-			}
-			survey.AskOne(prompt, &selecionada)
-
-			switch selecionada {
-			case "Conta Corrente":
-				tipo = 1
-			case "Conta Poupança":
-				tipo = 2
-			}
-
-			fmt.Println("\nDigite o nome do titular da conta: ")
-			titular, _ = reader.ReadString('\n')
-			titular = strings.TrimSpace(titular)
-
-			fmt.Println("\nDigite o saldo da conta: ")
-			fmt.Scanln(&saldo)
-
-			var resposta error
-
-			switch tipo {
-			case 1:
-
-				fmt.Println("\nDigite o limite da conta: ")
-				fmt.Scanln(&limite)
-
-				resposta = contaController.Criar(model.NewContaCorrente(0, agencia, tipo, titular, saldo, limite))
-
-			case 2:
-
-				fmt.Println("\nDigite o dia do aniversário da conta: ")
-				fmt.Scanln(&aniversario)
-
-				resposta = contaController.Criar(model.NewContaPoupanca(0, agencia, tipo, titular, saldo, aniversario))
-
-			}
-
-			if resposta == nil {
-				color.Set(color.FgGreen)
-				fmt.Printf("\nConta %s criada com sucesso!\n", selecionada)
-				color.Unset()
-			} else {
-				color.Set(color.FgRed)
-				fmt.Printf("\nErro ao criar conta: %s\n", resposta)
-				color.Unset()
-			}
-
-			keyPress()
+			criarConta(contaController, reader)
 		case 2:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nListar todas as Contas\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO LISTAR TODAS AS CONTAS */
-
-			contas, err := contaController.ListarTodas()
-
-			if err != nil {
-				color.Set(color.FgRed)
-				fmt.Println("Error:", err)
-				color.Unset()
-				return
-			}
-
-			for _, conta := range contas {
-				switch c := conta.(type) {
-				case *model.ContaCorrente:
-					c.Visualizar()
-				case *model.ContaPoupanca:
-					c.Visualizar()
-				default:
-					color.Set(color.FgRed)
-					fmt.Println("\nTipo de Conta Inválida")
-					color.Unset()
-				}
-			}
-
-			color.Set(color.FgCyan)
-			fmt.Printf("\nTotal de Contas Cadastradas: %d", len(contas))
-			color.Unset()
-
-			keyPress()
+			listarContas(contaController)
 		case 3:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nConsultar dados da Conta - por número\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO CONSULTAR CONTA PELO NÚMERO */
-
-			fmt.Println("Digite o número da conta: ")
-			fmt.Scanln(&numero)
-
-			conta, err := contaController.BuscarPorNumero(numero)
-
-			if err == nil {
-				switch c := conta.(type) {
-				case *model.ContaCorrente:
-					c.Visualizar()
-				case *model.ContaPoupanca:
-					c.Visualizar()
-				default:
-					color.Set(color.FgRed)
-					fmt.Println("\nTipo de Conta Inválida")
-					color.Unset()
-				}
-			} else {
-
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-
-			}
-
-			keyPress()
+			consultarConta(contaController)
 		case 4:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nAtualizar dados da Conta\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO ATUALIZAR DADOS DE UMA CONTA */
-
-			fmt.Println("Digite o número da conta: ")
-			fmt.Scanln(&numero)
-
-			conta, err := contaController.BuscarPorNumero(numero)
-
-			if err == nil {
-
-				switch c := conta.(type) {
-				case *model.ContaCorrente:
-					tipo = c.GetTipo()
-				case *model.ContaPoupanca:
-					tipo = c.GetTipo()
-				default:
-					color.Set(color.FgRed)
-					fmt.Println("\nTipo de Conta Inválida")
-					color.Unset()
-				}
-
-				fmt.Println("\nDigite o número da agência: ")
-				fmt.Scanln(&agencia)
-
-				fmt.Println("\nDigite o nome do titular da conta: ")
-				titular, _ = reader.ReadString('\n')
-				titular = strings.TrimSpace(titular)
-
-				fmt.Println("\nDigite o saldo da conta: ")
-				fmt.Scanln(&saldo)
-
-				var resposta error
-
-				switch tipo {
-				case 1:
-
-					fmt.Println("\nDigite o limite da conta: ")
-					fmt.Scanln(&limite)
-
-					resposta = contaController.Atualizar(model.NewContaCorrente(numero, agencia, tipo, titular, saldo, limite))
-
-				case 2:
-
-					fmt.Println("\nDigite o dia do aniversário da conta: ")
-					fmt.Scanln(&aniversario)
-
-					resposta = contaController.Atualizar(model.NewContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario))
-
-				}
-
-				if resposta == nil {
-					color.Set(color.FgGreen)
-					fmt.Printf("Conta Número %d atualizada com sucesso!\n", numero)
-					color.Unset()
-				} else {
-					color.Set(color.FgRed)
-					fmt.Printf("\n%s\n", resposta)
-					color.Unset()
-				}
-
-			} else {
-
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-
-			}
-
-			keyPress()
+			atualizarConta(contaController, reader)
 		case 5:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nApagar uma Conta\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO EXCLUIR CONTA */
-
-			fmt.Println("Digite o número da conta: ")
-			fmt.Scanln(&numero)
-
-			err := contaController.Deletar(numero)
-
-			if err != nil {
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-			} else {
-				color.Set(color.FgGreen)
-				fmt.Printf("\n\nConta Número %d excluída com sucesso!", numero)
-				color.Unset()
-			}
-
-			keyPress()
+			apagarConta(contaController)
 		case 6:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nSaque\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO SACAR */
-
-			fmt.Println("Digite o número da conta: ")
-			fmt.Scanln(&numero)
-
-			fmt.Println("Digite o valor do saque (R$): ")
-			fmt.Scanln(&valor)
-
-			err := contaController.Sacar(numero, valor)
-
-			if err != nil {
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-			}else{
-
-				color.Set(color.FgGreen)
-				fmt.Printf("\nO Saque no valor de R$ %.2f, foi realizado com sucesso!", valor)
-				color.Unset()
-
-			}
-
-			keyPress()
+			realizarSaque(contaController)
 		case 7:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nDepósito\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO DEPOSITAR */
-
-			fmt.Println("Digite o número da conta: ")
-			fmt.Scanln(&numero)
-
-			fmt.Println("Digite o valor do depósito (R$): ")
-			fmt.Scanln(&valor)
-
-			err := contaController.Depositar(numero, valor)
-
-			if err != nil {
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-			} else {
-				color.Set(color.FgGreen)
-				fmt.Printf("\n\nO depósito de R$ %.2f, na conta Número %d, foi efetuado com sucesso!", valor, numero)
-				color.Unset()
-			}
-
-			keyPress()
+			realizarDeposito(contaController)
 		case 8:
-			color.Set(color.FgYellow, color.Bold)
-			fmt.Print("\n\nTransferência entre Contas\n\n")
-			color.Unset()
-
-			/* CHAMADA DO MÉTODO TRANSFERIR */
-
-			fmt.Println("Digite o número da conta de origem: ")
-			fmt.Scanln(&numero)
-
-			fmt.Println("Digite o número da conta de destino: ")
-			fmt.Scanln(&numeroDestino)
-
-			fmt.Println("Digite o valor do saque (R$): ")
-			fmt.Scanln(&valor)
-
-			err := contaController.Transferir(numero, numeroDestino, valor)
-
-			if err != nil {
-				color.Set(color.FgRed)
-				fmt.Printf("\n%s", err)
-				color.Unset()
-			} else {
-				color.Set(color.FgGreen)
-				fmt.Printf("\n\nA Transferência no valor de R$ %.2f, para a conta Número %d, foi efetuada com sucesso!", valor, numeroDestino)
-				color.Unset()
-			}
-
-			keyPress()
+			realizarTransferencia(contaController)
+		case 9:
+			encerrarPrograma()
 		default:
 			color.Set(color.FgRed, color.Bold)
 			fmt.Print("\nOpção Inválida!\n")
 			color.Unset()
-
-			keyPress()
 		}
 
+		keyPress()
 	}
+}
+
+func inicializaContas(contaController repository.IContaRepository) {
+	contaController.Criar(model.NewContaCorrente(0, 123, 1, "João da Silva", 1000.00, 100.00))
+	contaController.Criar(model.NewContaCorrente(0, 456, 1, "Maria dos Santos", 2900.00, 300.00))
+	contaController.Criar(model.NewContaPoupanca(0, 789, 2, "Giovanna Benvenutti", 3200.00, 10))
+	contaController.Criar(model.NewContaPoupanca(0, 123, 2, "Karina Girardini", 2000.00, 15))
+}
+
+func exibirMenu() {
+
+	color.Set(color.BgBlack, color.FgYellow)
+	fmt.Println("\n*****************************************************")
+	fmt.Println("                BANCO DO BRAZIL COM Z                ")
+	fmt.Println("*****************************************************")
+	fmt.Println("            1 - Criar Conta                          ")
+	fmt.Println("            2 - Listar todas as Contas               ")
+	fmt.Println("            3 - Buscar Conta por Numero              ")
+	fmt.Println("            4 - Atualizar Dados da Conta             ")
+	fmt.Println("            5 - Apagar Conta                         ")
+	fmt.Println("            6 - Sacar                                ")
+	fmt.Println("            7 - Depositar                            ")
+	fmt.Println("            8 - Transferir valores entre Contas      ")
+	fmt.Println("            9 - Sair                                 ")
+	fmt.Println("*****************************************************")
+	color.Unset()
+}
+
+func readOption() int {
+	var opcao int
+	fmt.Print("\nDigite a opção desejada: ")
+	fmt.Scanln(&opcao)
+	return opcao
+}
+
+func criarConta(contaController repository.IContaRepository, reader *bufio.Reader) {
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nCriar Conta\n\n")
+	color.Unset()
+
+	agencia := readInt("\nDigite o número da agência: ")
+	tipo := selectTipoConta()
+	titular := readString(reader, "\nDigite o nome do titular da conta: ")
+	saldo := readFloat("\nDigite o saldo da conta: ")
+
+	var resposta error
+
+	if tipo == 1 {
+		limite := readFloat("Digite o limite da conta: ")
+		resposta = contaController.Criar(model.NewContaCorrente(0, agencia, tipo, titular, saldo, limite))
+	} else {
+		aniversario := readInt("Digite o dia do aniversário da conta: ")
+		resposta = contaController.Criar(model.NewContaPoupanca(0, agencia, tipo, titular, saldo, aniversario))
+	}
+
+	if resposta == nil {
+		color.Green("\nConta criada com sucesso!")
+	} else {
+		color.Red("\nErro ao criar conta: %s", resposta)
+	}
+}
+
+func listarContas(contaController repository.IContaRepository) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nListar todas as Contas\n\n")
+	color.Unset()
+
+	contas, err := contaController.ListarTodas()
+
+	if err != nil {
+		color.Red("Error: %s", err)
+		return
+	}
+
+	for _, conta := range contas {
+        if c, ok := conta.(Banco); ok {
+            c.Visualizar()
+        } else {
+            color.Red("\nTipo de Conta Inválida")
+        }
+    }
+
+	color.Cyan("\nTotal de Contas Cadastradas: %d", len(contas))
+}
+
+func consultarConta(contaController repository.IContaRepository) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nConsultar dados da Conta - por número\n\n")
+	color.Unset()
+
+	numero := readInt("Digite o número da conta: ")
+	conta, err := contaController.BuscarPorNumero(numero)
+
+	if err == nil {
+		if c, ok := conta.(Banco); ok {
+            c.Visualizar()
+        } else {
+            color.Red("\nTipo de Conta Inválida")
+        }
+	} else {
+		color.Red("\n%s", err)
+	}
+}
+
+func atualizarConta(contaController repository.IContaRepository, reader *bufio.Reader) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nAtualizar dados da Conta\n\n")
+	color.Unset()
+
+	numero := readInt("Digite o número da conta: ")
+	conta, err := contaController.BuscarPorNumero(numero)
+
+	if err == nil {
+		agencia := readInt("\nDigite o número da agência: ")
+		titular := readString(reader, "\nDigite o nome do titular da conta: ")
+		saldo := readFloat("\nDigite o saldo da conta: ")
+
+		var resposta error
+		switch c := conta.(type) {
+			case *model.ContaCorrente:
+				limite := readFloat("\nDigite o limite da conta: ")
+				resposta = contaController.Atualizar(model.NewContaCorrente(numero, agencia, c.GetTipo(), titular, saldo, limite))
+			case *model.ContaPoupanca:
+				aniversario := readInt("\nDigite o dia do aniversário da conta: ")
+				resposta = contaController.Atualizar(model.NewContaPoupanca(numero, agencia, c.GetTipo(), titular, saldo, aniversario))
+			default:
+				color.Red("\nTipo de Conta Inválida")
+				return
+		}
+
+		if resposta == nil {
+			color.Green("Conta Número %d atualizada com sucesso!", numero)
+		} else {
+			color.Red("\n%s", resposta)
+		}
+	} else {
+		color.Red("\n%s", err)
+	}
+}
+
+func apagarConta(contaController repository.IContaRepository) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nApagar uma Conta\n\n")
+	color.Unset()
+
+	numero := readInt("Digite o número da conta: ")
+	err := contaController.Deletar(numero)
+
+	if err != nil {
+		color.Red("\n%s", err)
+	} else {
+		color.Green("\nConta Número %d excluída com sucesso!", numero)
+	}
+}
+
+func realizarSaque(contaController repository.IContaRepository) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nSaque\n\n")
+	color.Unset()
+
+	numero := readInt("Digite o número da conta: ")
+	valor := readFloat("Digite o valor do saque (R$): ")
+
+	err := contaController.Sacar(numero, valor)
+
+	if err != nil {
+		color.Red("\n%s", err)
+	} else {
+		color.Green("\nO Saque no valor de R$ %.2f, foi realizado com sucesso!", valor)
+	}
+}
+
+func realizarDeposito(contaController repository.IContaRepository) {
+
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nDepósito\n\n")
+	color.Unset()
+
+	numero := readInt("Digite o número da conta: ")
+	valor := readFloat("Digite o valor do depósito (R$): ")
+
+	err := contaController.Depositar(numero, valor)
+
+	if err != nil {
+		color.Red("\n%s", err)
+	} else {
+		color.Green("\nO depósito de R$ %.2f, na conta Número %d, foi efetuado com sucesso!", valor, numero)
+	}
+}
+
+func realizarTransferencia(contaController repository.IContaRepository) {
+	
+	color.Set(color.FgYellow, color.Bold)
+	fmt.Print("\n\nTransferência entre Contas\n\n")
+	color.Unset()
+
+	numeroOrigem := readInt("Digite o número da conta de origem: ")
+	numeroDestino := readInt("Digite o número da conta de destino: ")
+	valor := readFloat("Digite o valor da transferência (R$): ")
+
+	err := contaController.Transferir(numeroOrigem, numeroDestino, valor)
+
+	if err != nil {
+		color.Red("\n%s", err)
+	} else {
+		color.Green("\nA Transferência no valor de R$ %.2f, para a conta Número %d, foi efetuada com sucesso!", valor, numeroDestino)
+	}
+}
+
+func encerrarPrograma() {
+	color.Cyan("\nBanco do Brazil com Z - O seu Futuro começa aqui!")
+	sobre()
+	os.Exit(0)
 }
 
 func sobre() {
@@ -396,7 +286,44 @@ func sobre() {
 }
 
 func keyPress() {
-	fmt.Println("\n\nPressione Enter para continuar...")
-	reader := bufio.NewReader(os.Stdin)
-	_, _ = reader.ReadString('\n')
+	fmt.Print("\nPressione Enter para continuar...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func readInt(prompt string) int {
+	var value int
+	fmt.Print(prompt)
+	fmt.Scanln(&value)
+	return value
+}
+
+func readFloat(prompt string) float64 {
+	var value float64
+	fmt.Print(prompt)
+	fmt.Scanln(&value)
+	return value
+}
+
+func readString(reader *bufio.Reader, prompt string) string {
+	fmt.Print(prompt)
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
+}
+
+func selectTipoConta() int {
+	selecionada := ""
+	prompt := &survey.Select{
+		Message: "Selecione o tipo da conta:",
+		Options: []string{"Conta Corrente", "Conta Poupança"},
+	}
+	survey.AskOne(prompt, &selecionada)
+
+	switch selecionada {
+	case "Conta Corrente":
+		return 1
+	case "Conta Poupança":
+		return 2
+	default:
+		return 0
+	}
 }
